dce7d0d0315235aa7a9c66b141a62dcd9a6260e9
hydyrayhan <hydyrayhan7@gmail.com>
2021-12-23 14:23:10 +0500

    ip change

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const host = process.env.HOST;
 // const host = 'http://192.168.27.142:5000';
 const app = express(); 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
 
 app.use(express.static("public"))
