3fa84caca4aa444c7ea848ce0f65e6adf8539807
hydyrayhan <hydyrayhan7@gmail.com>
2022-01-05 22:02:04 +0500

    asdf

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const fs = require("fs")
 require("dotenv").config({path:"./config/config.env"});
 const path = require('path')
-const host = process.env.HOST+"/api";
+const host = process.env.HOST;
 const hostiso = process.env.HOSTISO;
 
 const app = express();
