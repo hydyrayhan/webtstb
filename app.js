3ab3807cd6a6277aa9bfee9990342943b8aa6bdc
hydyrayhan <hydyrayhan7@gmail.com>
2022-01-05 22:24:51 +0500

    sdfsdf

--- app.js
+++ app.js
@@ -34,9 +34,7 @@ app.get('/',async function(req,res){
     // mainPage = await axios.get(host);
     mainPage = await axios({
       method:"get",
-      url:`https://tstb.gov.tm:5003`
-  }).then(data=>{
-    console.log("data:"+data);
+      url:`http://tstb.gov.tm:5000`
   }).catch(err=>console.log(`test ${err}`));
   }catch(error){
     console.log("test catch:"+error)
