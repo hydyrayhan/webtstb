75f37c08af192f11d847b29a5def8bb4fb516ab9
hydyrayhan <hydyrayhan7@gmail.com>
2022-01-05 19:26:39 +0500

    fdfd

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const axios = require('axios');
 const bodyParser = require("body-parser");
 const fs = require("fs")
 require("dotenv").config({path:"./config/config.env"});
-const host = process.env.HOST;
+// const host = process.env.HOST;
 const hostiso = process.env.HOSTISO;
-// const host = 'https://tstb.gov.tm:5003'
+const host = 'https://tstb.gov.tm:5003'
 // const host = 'http://192.168.1.104:5000'
 // const host = 'http://10.192.168.51:5000';
 // const host = 'http://192.168.27.142:5000';
@@ -33,7 +33,7 @@ app.get('/',async function(req,res){
  var sl = req.query.sl;
  var mainPage;
   try{
-    mainPage = await axios.get(host+"/");
+    mainPage = await axios.get(host);
   }catch(error){
     console.log(error)
   }
