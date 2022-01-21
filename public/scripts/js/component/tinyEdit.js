29e098355cf1f2610231f80a5c93f3b8136cffc8
hydyrayhan <hydyrayhan7@gmail.com>
2022-01-21 17:54:30 +0500

    tiny editor add

---
+++ public/scripts/js/component/tinyEdit.js
@@ -0,0 +1,154 @@
+function setData(data1,data2,data3){
+  tinymce.init({
+    selector: 'textarea#image-tools',
+    // content_css:'dark',
+    setup: function (editor) {
+      editor.on('init', function (e) {
+        editor.setContent(data1);
+      });
+    },
+    height: 500,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste imagetools wordcount',
+    ],
+    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+    image_title: true,
+    
+    automatic_uploads: true,
+    
+    file_picker_types: 'image',
+    
+    file_picker_callback: function (cb, value, meta) {
+      var input = document.createElement('input');
+      input.setAttribute('type', 'file');
+      input.setAttribute('accept', 'image/*');
+  
+  
+      input.onchange = function () {
+        var file = this.files[0];
+  
+        var reader = new FileReader();
+        reader.onload = function () {
+          var id = 'blobid' + (new Date()).getTime();
+          var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+          var base64 = reader.result.split(',')[1];
+          var blobInfo = blobCache.create(id, file, base64);
+          blobCache.add(blobInfo);
+  
+          cb(blobInfo.blobUri(), { title: file.name });
+        };
+        reader.readAsDataURL(file);
+      };
+  
+      input.click();
+    },
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+  });
+  
+  
+  
+  //--------------------------------------------------------------------------------------------
+  tinymce.init({
+    selector: 'textarea#image-tools2',
+    // content_css:'dark',
+    setup: function (editor) {
+      editor.on('init', function (e) {
+        editor.setContent(data2);
+      });
+    },
+    height: 500,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste imagetools wordcount',
+    ],
+    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+    image_title: true,
+    
+    automatic_uploads: true,
+    
+    file_picker_types: 'image',
+    
+    file_picker_callback: function (cb, value, meta) {
+      var input = document.createElement('input');
+      input.setAttribute('type', 'file');
+      input.setAttribute('accept', 'image/*');
+  
+  
+      input.onchange = function () {
+        var file = this.files[0];
+  
+        var reader = new FileReader();
+        reader.onload = function () {
+          var id = 'blobid' + (new Date()).getTime();
+          var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+          var base64 = reader.result.split(',')[1];
+          var blobInfo = blobCache.create(id, file, base64);
+          blobCache.add(blobInfo);
+  
+          cb(blobInfo.blobUri(), { title: file.name });
+        };
+        reader.readAsDataURL(file);
+      };
+  
+      input.click();
+    },
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+  });
+  
+  
+  
+  //===============================================================================================================
+  
+  
+  tinymce.init({
+    selector: 'textarea#image-tools3',
+    // content_css:'dark',
+    setup: function (editor) {
+      editor.on('init', function (e) {
+        editor.setContent(data3);
+      });
+    },
+    height: 500,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste imagetools wordcount',
+    ],
+    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+    image_title: true,
+    
+    automatic_uploads: true,
+    
+    file_picker_types: 'image',
+    
+    file_picker_callback: function (cb, value, meta) {
+      var input = document.createElement('input');
+      input.setAttribute('type', 'file');
+      input.setAttribute('accept', 'image/*');
+  
+  
+      input.onchange = function () {
+        var file = this.files[0];
+  
+        var reader = new FileReader();
+        reader.onload = function () {
+          var id = 'blobid' + (new Date()).getTime();
+          var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+          var base64 = reader.result.split(',')[1];
+          var blobInfo = blobCache.create(id, file, base64);
+          blobCache.add(blobInfo);
+  
+          cb(blobInfo.blobUri(), { title: file.name });
+        };
+        reader.readAsDataURL(file);
+      };
+  
+      input.click();
+    },
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+  });
+  
+}
